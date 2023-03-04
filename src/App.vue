7db8bab5c4dfd5e6808837ef9962a4974497a5a4
Adika Suta <adika.suta@online-pajak.com>
2023-03-04 18:05:09 +0700

    fix layouting

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,40 @@
 <template>
   <v-app>
+    <HeaderComponent />
+    <SidebarComponent v-if="true" />
     <v-main>
-      <HeaderComponent>
+      <v-sheet id="scrolling-techniques-7" class="overflow-y-auto">
         <router-view />
-      </HeaderComponent>
-      <FooterComponent></FooterComponent>
+        <FooterComponent />
+      </v-sheet>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HeaderComponent from './components/layout/Header'
-import FooterComponent from './components/layout/Footer'
-import { localize } from 'vee-validate';
+import HeaderComponent from "./components/layout/Header";
+import SidebarComponent from "./components/layout/Sidebar";
+import FooterComponent from "./components/layout/Footer";
+import { localize } from "vee-validate";
 export default {
-  name: 'App',
-  components: { HeaderComponent, FooterComponent },
-  data: () => ({
-  }),
+  name: "App",
+  components: { HeaderComponent, SidebarComponent, FooterComponent },
+  data: () => ({}),
   methods: {
     initLanguage: function () {
-      const lang = window.$cookies.get('LANG');
-      if(!lang){
-        window.$cookies.set('LANG','id');
+      const lang = window.$cookies.get("LANG");
+      if (!lang) {
+        window.$cookies.set("LANG", "id");
       }
-      this.$i18n.locale = window.$cookies.get('LANG')
-      localize(window.$cookies.get('LANG'))
-    }
+      this.$i18n.locale = window.$cookies.get("LANG");
+      localize(window.$cookies.get("LANG"));
+    },
   },
   created() {
-    this.initLanguage()
+    this.initLanguage();
   },
   mounted() {
-    this.initLanguage()
-  }
+    this.initLanguage();
+  },
 };
 </script>
---
+++ src/views/internal-dashboard/Workflow.vue
@@ -0,0 +1,30 @@
+<template>
+    
+</template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        counter: 0,
+      };
+    },
+    methods: {
+      goToCart(){
+              this.$router.push('/Cart')
+          },
+      next() {
+        this.counter++;
+        this.$emit(this.counter);
+      },
+      previous() {
+        this.counter--;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+
+  </style>
+  
